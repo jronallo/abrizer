74b345fe3db3c6334dec9d9b5ff23e82f224912a
Jason Ronallo <jronallo@gmail.com>
2017-04-08 18:15:46 -0400

    add message for ffprobe error

--- lib/abrizer/errors.rb
+++ lib/abrizer/errors.rb
@@ -3,13 +3,17 @@ module Abrizer
   # This error is raised if Abrizer::ReadAdaptations is unable to determine
   # the adaptations that have or will be created for a given mezzanine file.
   class ReadAdaptationsError < StandardError
-    def initialize(msg=nil)
-      msg ||= "Unable to read adaptations. Either inlude the path to a master file or an identifier. If passing just the identifier you must have created an adaptations.json file to read in or as a final fallback have already created a VP9 derivative."
+    def initialize
+      msg = "Unable to read adaptations. Either inlude the path to a master file or an identifier. If passing just the identifier you must have created an adaptations.json file to read in or as a final fallback have already created a VP9 derivative."
       super(msg)
     end
   end
 
   class FfprobeError < StandardError
+    def initialize
+      msg = "Unable to find file to run ffprobe on or unable to read in ffprobe.json file."
+      super(msg)
+    end
   end
 
   class Mp4AdaptationNotFoundError < StandardError
