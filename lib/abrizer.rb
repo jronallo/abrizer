670271742d703424f21252f96045221bfc84c327
Jason Ronallo <jronallo@gmail.com>
2017-04-08 17:12:32 -0400

    easier reading abrizer library dependencies required

--- lib/abrizer.rb
+++ lib/abrizer.rb
@@ -2,37 +2,42 @@ require 'yajl'
 require 'multi_json'
 MultiJson.use :yajl
 MultiJson.dump_options = {pretty: true}
+
 require 'jbuilder'
 require 'video_sprites'
 require 'ostruct'
-require "abrizer/version"
-require 'abrizer/filepath_helpers'
-require 'abrizer/read_adaptations'
-require 'abrizer/debug_settings'
-require 'abrizer/identifier_helpers'
-require 'abrizer/information_helpers'
-require 'abrizer/package_dash_shaka'
-require 'abrizer/package_hls_shaka'
-require 'abrizer/package_dash_bento'
-require 'abrizer/package_hls_bento'
-require 'abrizer/adaptation'
-require 'abrizer/adaptation_finder'
-require 'abrizer/adaptations_file'
-require 'abrizer/ffmpeg_processor'
-require 'abrizer/ffprobe_informer'
-require 'abrizer/ffprobe_file'
-require 'abrizer/processor'
-require 'abrizer/cleaner'
-require 'abrizer/progressive_mp4'
-require 'abrizer/progressive_vp9'
-require 'abrizer/progressive_mp3'
-require 'abrizer/sprites'
-require 'abrizer/captions'
-require 'abrizer/canvas'
-require 'abrizer/data'
-require 'abrizer/temporary_poster'
-require 'abrizer/all'
 
+%w[
+  version
+  filepath_helpers
+  read_adaptations
+  debug_settings
+  identifier_helpers
+  information_helpers
+  package_dash_shaka
+  package_hls_shaka
+  package_dash_bento
+  package_hls_bento
+  adaptation
+  adaptation_finder
+  adaptations_file
+  ffmpeg_processor
+  ffprobe_informer
+  ffprobe_file
+  processor
+  cleaner
+  progressive_mp4
+  progressive_vp9
+  progressive_mp3
+  sprites
+  captions
+  canvas
+  data
+  temporary_poster
+  all
+].each do |dependency|
+  require "abrizer/#{dependency}"
+end
 
 
 module Abrizer
